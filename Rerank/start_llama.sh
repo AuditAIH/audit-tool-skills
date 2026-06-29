11dd75a899a442da4ad61b5f8593ad4caf81e7db
AuditAIH <145266260+AuditAIH@users.noreply.github.com>
2026-06-29 14:41:53 +0800

    Modify start_llama.sh for llama-server and CUDA 13
    Updated the script to use the built-in llama-server and specified the CUDA 13 backend environment. Added instructions for model pulling and optional parameters for debugging.

--- Rerank/start_llama.sh
+++ Rerank/start_llama.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# /root/models/bge-reranker-v2-m3-GGUF/bge-reranker-v2-m3-FP16.gguf
-# /root/llama.cpp/Qwen3-Reranker-0.6B-Q8_0-GGUF/qwen3-reranker-0.6b-q8_0.gguf
+# 替换为ollama内置llama-server，直接指定cuda13后端环境，按需调整为v12
+# 请先运行ollama pull AuditAid/Qwen3_Reranker #手动拉取模型
+# 按需增加`--ubatch-size 4096  --batch-size 4096`参数，开启调试  --verbose
+export GGML_BACKEND_PATH=/usr/local/lib/ollama/cuda_v13/libggml-cuda.so
+export LD_LIBRARY_PATH=/usr/local/lib/ollama:/usr/local/lib/ollama/cuda_v13
+export CUDA_VISIBLE_DEVICES=0
 
-/root/llama.cpp/build/bin/llama-server \
-  --model /root/llama.cpp/Qwen3-Reranker-0.6B-Q8_0-GGUF/qwen3-reranker-0.6b-q8_0.gguf \
+/usr/local/lib/ollama/llama-server \
+  --model /usr/share/ollama/.ollama/models/blobs/sha256-22c9979ce4fbcdc5acdc310c6641c32797eff1aa980b8f7a2db8a8ea23429a48 \
   --host 0.0.0.0 \
   --port 11435 \
   --no-webui \
   --rerank \
   --ctx-size 8192 \
-  --n-gpu-layers 99 \
-  --verbose
+  --n-gpu-layers 99
